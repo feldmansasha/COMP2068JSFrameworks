ebf3ce047456d9ba6b72b8cdec052e3809d44d8c
Alex Feldman <feldmansasha5@gmail.com>
2024-10-17 18:24:59 -0400

    Second push 80% completed
    Done the landing and about pages. Improved size of the header. Checked all hbs view in W3 validator and corrected all errors. Final review and polish of css left and publishing to the open server

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Alex Feldman' });
 });
 
 router.get('/about', function(req, res, next){
-  res.render('about', {title:'About me page'});
+  res.render('about', {title:'About me'});
 })
 
 router.get('/projects', function(req, res, next){
-  res.render('projects');
+  res.render('projects', {title: "Alex's projects"});
 })
 
 router.get('/contact', function(req, res, next){
-  res.render('contact');
+  res.render('contact', {title:'Contact me'});
 })
 
 module.exports = router;
